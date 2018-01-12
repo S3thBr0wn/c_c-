42248714cac6bc12370b7a0f95907d76d2e84f41
S3thBr0wn <31544947+S3thBr0wn@users.noreply.github.com>
2018-01-12 20:45:39 +0000

    Update squarefunction.c

--- squarefunction.c
+++ squarefunction.c
@@ -1,22 +1,22 @@
 // ******* squarefunction.c *******
 		
-	#include <stdio.h>
-	int square ( int v );
-		
-	int main( )
-	{
-		int x, n ;
-		printf ( "\nEnter some number for finding square \n");
-		scanf ( "%d", &n) ;
-		x = square ( n ) ;
-		printf ( "nSquare of the given number %d is %d", n, x );
-		printf("\n\n");
-		return 0;
-	}
-		
- int square ( int v )
- {
-	 int p ;
-	 p = v * v ;
-	 return ( p ) ;
- }
+#include <stdio.h>
+int square ( int v );
+
+int main( )
+{
+	int x, n ;
+	printf ( "\nEnter some number for finding square \n");
+	scanf ( "%d", &n) ;
+	x = square ( n ) ;
+	printf ( "\nSquare of the given number %d is %d", n, x );
+	printf("\n\n");
+	return 0;
+}
+
+int square ( int v )	//function definition	
+{
+	int p ;
+	p = v * v ;
+	return ( p ) ;
+}
