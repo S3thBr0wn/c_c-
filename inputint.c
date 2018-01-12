c949a4fbb35beb2a105691b544ef5811187d6e19
S3thBr0wn <31544947+S3thBr0wn@users.noreply.github.com>
2018-01-12 20:42:32 +0000

    Update inputint.c

--- inputint.c
+++ inputint.c
@@ -6,7 +6,7 @@
 			
 			printf("Type in a number \n");
 			scanf("%d", &number);
-			printf("The number you typed was %d/n", number);
+			printf("The number you typed was %d\n", number);
 			x = number * number ;
 			printf("number * number is %d\n" , x);
 			return 0;
